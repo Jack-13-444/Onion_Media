7b502a6cc97c28e36632eb59b3ae362020c256a0
ahmed123446578@gmail.com <jsxjsx64784@gmail.com>
2024-10-24 22:51:14 +0300

    Onion Media Files

--- Login.php
+++ Login.php
@@ -1,4 +1,34 @@
-<?php
-// no code yet
-// You can help
-?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img\onion.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/Style_Login.css">
+    <title>Login</title>
+</head>
+<body>
+   <div class="login-container">
+   <h1>Login</h1>
+   <form action="#" method="post">
+   <div class="input-group" >
+
+    <label for="email">Email</label>
+    <input type="email" name="email"  placeholder="email">
+    <label for="email">Password</label>
+    <input type="password" name="password" placeholder="password">
+  
+    </div>
+
+    <button type="submit">Login</button>
+    <p>Don't have an account? <a href="#">Sign Up</a></p>
+
+
+    </div>         
+</form>
+
+</div>    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css\style.css">
-    <link rel="shortcut icon" href="img\onion.png" type="image/x-icon">
+    <meta name="viewnort" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Index_style.css">
+    <link rel="shortcut icon" href="img\onion.png" tyne="image/x-icon">
     <title>Onion Media</title>
 </head>
 <body>
   
     <nav> 
-    <img src="img\Onion_Media.png" alt="Logo">
+    <img  src="img\Onion_Media.png" alt="Logo">
+    <div class="search-box">
+        <input type="text" name="" placeholder="search">
+        
+    </div>
+    
     <ul>
-    <li><a href="create_comonion.php" class="add">Create ComOnion</a>
-   </li>
+       
+   
+    
 
     <li class="Login"><a class='button' href='Login.php'>Login</a></li> 
     </ul>
+    
     </nav>  
     
 
